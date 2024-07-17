aa7526f2c78d60b51314319a2dd88b7c5ef2ade0
Damie Pak <damiepak@Damies-Air.cable.rcn.com>
2024-07-17 07:30:30 -0400

    Working dispersal matrix

--- Code/loading_packages.R
+++ Code/loading_packages.R
@@ -4,4 +4,5 @@ library(ggplot2)
 library(gganimate)
 library(reshape2) 
 library(readr)
-
+library(Rcpp)
+library(viridis)
--- Code/simulator.R
+++ Code/simulator.R
@@ -1,106 +1,89 @@
 Simulator_function <- function(num_patches,
                                initial_values = "default",
                                parameter_values = "default",
+                               disturbance = 'default', 
                                times){
         
-        
+        ###We create a scale-free network
         new_network <-  erdos.renyi.game(
                 num_patches,
-                0.35,
+                0.5,
                 type = c("gnp"),
                 directed = FALSE,
                 loops = FALSE
         )
         
+        ###Giving a specific name to the patches name
         new_network <- set_vertex_attr(new_network, "name", 
                                        value=seq(1,num_patches))
         
-        
+        ###This gives me the adjacency matrix
         adj_matrix <- as_adjacency_matrix(new_network, sparse = FALSE)
         
-        initial_y <- c(HS = rep(100,num_patches),
-                       HI = rep(0,num_patches),
-                       HR = rep(0,num_patches),
-                       PS = sample.int(num_patches , replace = TRUE) * 100,
-                       PI = rep(0,num_patches),
-                       SS = sample.int(num_patches , replace = TRUE) * 100,
-                       SI = rep(0,num_patches))
         
+        ###The initial conditions
+        initial_y <- c(HS = rep(1,num_patches),
+                       HI = rep(5,num_patches),
+                       HR = rep(0,num_patches),
+                       PS = runif(num_patches) * 0.1,
+                       PI = runif(num_patches) * 0,
+                       SS = runif(num_patches) * 0.4,
+                       SI = runif(num_patches) * 0)
         
         
         parameters_n <- c(
-                b_H = 0.05, #Human birth rate
-                b_P = 11, #P.vector birth rate
-                b_S = 11, #S. vector birth rate
+                b_H = 0, #Human birth rate
+                b_P = 0.001, #P.vector birth rate
+                b_S = 0.005, #S. vector birth rate
                 mu_H = 1/80, #Human death rate
-                mu_P = 0.02, #P. vector death rate
-                mu_S = 0.04, #S. vector death rate
+                mu_P = 0.001, #P. vector death rate
+                mu_S = 0.005, #S. vector death rate
                 
-                a_P = 0, #biting rate of the p. vector
-                a_S = 0, #biting rate of the s.vector
+                a_P = 0.0, #biting rate of the p. vector
+                a_S = 0.0, #biting rate of the s.vector
                 
-                phi_P = 0.40, #transmission probability of p. vector
+                phi_P = 0.10, #transmission probability of p. vector
                 phi_S = 0.10, #transmission probability of s. vector
-                phi_H  = 0.90, #transmission probability of human
+                phi_H  = 0.30, #transmission probability of human
                 
                 # Recovery rate
                 gamma = 1/7,  #recovery rate of infected human
                 
                 #competition coefficient
-                c_PS = 0, #competitition effect of p.vector on s.vector
-                c_SP = 0  #competitition effect of s.vector on p.vector
+                c_PS = 0.05, #competitition effect of p.vector on s.vector
+                c_SP = 0.01  #competitition effect of s.vector on p.vector
         )
         
-        df.contact <- adj_matrix * 0 
+        
+        df.contact <- adj_matrix 
         
         results <- data.frame(deSolve::lsoda(
-                times = 1:120,
+                times = seq(1,1000,1),
                 y = initial_y ,
                 func = trito_metapop,
                 parms = parameters_n,
                 patch_num =  num_patches ,
-                disp_mat =df.contact 
+                disp_mat = df.contact 
                 
         ))
         
-        primary_infected <- cbind(time=results[,'time'],
-                                  results [ , grepl( "PS" , names( results  ) ) ])
         
-        secondary_infected <- cbind(time= results[,'time'],
-                                    results[,grepl("SS", names(results))])
-        
-        
-        primary_mat <- as.matrix(primary_infected[,2:(num_patches+ 1)])
-        
-        
-        secondary_mat <- as.matrix(secondary_infected[,2:(num_patches+ 1)])
-        
-        
-        primary_secondary_ratio <- cbind.data.frame(time = results[,'time'],
-                                                    primary_mat/secondary_mat)
-        
-        
-        primary_secondary_ratio_Melted <- melt(primary_secondary_ratio, id.vars ='time')
-        primary_secondary_ratio_Melted$Patch_num <- parse_number(as.character(primary_secondary_ratio_Melted$variable))
-        
-        
-        
-        
+        PSV_df <- calculate_PSV_ratio(results)
         df_network <- fortify(new_network)
         nodes_coord <-  distinct(df_network,x,y,name)
         
-        full_PI <- left_join(nodes_coord, primary_secondary_ratio_Melted, by=c("name" = "Patch_num"))
+        full_PI <- left_join(nodes_coord, PSV_df, by=c("name" = "Patch_num"))
         
         a_gif <- ggplot(data = df_network)+
                 geom_segment(aes( x=x, xend = xend, y = y, yend = yend))+
-                geom_point(data=full_PI,aes(x=x,y=y,color = value),size = 8)+
+                geom_point(data=full_PI,aes(x=x,y=y,color = log(value),size = log(value)))+
                 scale_color_viridis(option = 'turbo')+
-                
-                theme_classic()+
+                scale_size_continuous(range=c(0.05,15))+
+                theme_void()+
                 labs(title = 'Values at {(as.integer(frame_time))}')+
                 transition_time(time)
         
-        animate(a_gif, height = 800, width =800)
+        animate(a_gif, height = 800, width =800,rewind = FALSE)
         anim_save("Gapminder_example.gif")
         
         
